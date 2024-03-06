40263fd4c216ba9e72c5148715342ac92a7b1917
Vinicius <vini2143vinicius@gmail.com>
2024-03-06 11:50:03 -0300

    feat: criado a main e login do bot

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+using DotNetEnv;
+
+public class Program
+{
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    private static DiscordSocketClient _client;
+
+    public static async Task Main()
+    {
+        _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        
+
+        //  You can assign your bot token to a string, and pass that in to connect.
+        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        Env.Load();
+        var token = Environment.GetEnvironmentVariable("TOKEN");
+
+        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+        // var token = File.ReadAllText("token.txt");
+        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+}
